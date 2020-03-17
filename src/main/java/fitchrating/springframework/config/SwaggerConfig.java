79e3d95b7bf21bba5ca71e59dca893e0de5acb6c
unknown <anuj.nxl@gmail.com>
2020-03-17 11:58:12 -0500

    Added changes by Ganesh for Swagger update

--- src/main/java/fitchrating/springframework/config/SwaggerConfig.java
+++ src/main/java/fitchrating/springframework/config/SwaggerConfig.java
@@ -32,7 +32,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .version("1.0.0")
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-                .contact(new Contact("Meghana Chappidi ", "https://fitchrating.com/", "meghna@gmail.com"))
+                .contact(new Contact("Ganesh ", "https://city.com/", "ganesh@gmail.com"))
                 .build();
     }
     @Override
